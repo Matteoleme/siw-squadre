6a84ac4b651090e55ad529f0e0e4c2de5a0f17fe
Matteoleme <matteo.mastranza01@gmail.com>
2024-01-20 19:36:35 +0100

    Create PresidenteRepository.java
    Aggiunta query in PresidenteRepository

---
+++ src/main/java/it/uniroma3/siw/repository/PresidenteRepository.java
@@ -0,0 +1,17 @@
+package it.uniroma3.siw.repository;
+
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.CrudRepository;
+
+import it.uniroma3.siw.model.Presidente;
+
+public interface PresidenteRepository extends CrudRepository<Presidente, Long> {
+
+	public boolean existsByCf(String cf);
+	
+	@Query(value="SELECT p.* "
+			+ "FROM presidente as p "
+			+ "LEFT JOIN squadra as s ON p.id = s.presidente_id ", nativeQuery=true)
+			//+ "WHERE s.presidente_id IS NULL")
+	public Iterable<Presidente>findPresidentiLiberi();
+}
