837e403640369736e42f5bbcb5f230caa51a32fb
Matteoleme <matteo.mastranza01@gmail.com>
2024-01-25 20:17:44 +0100

    Ora si possono aggiungere i giocatori
    Create GiocatoreController e Validator per gestire l'aggiunta dei giocatori nel database. Create anche due pagine html per la form di invio e visualizzazione dettaglio giocatore

---
+++ src/main/java/it/uniroma3/siw/controller/GiocatoreController.java
@@ -0,0 +1,38 @@
+package it.uniroma3.siw.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import it.uniroma3.siw.model.Giocatore;
+import it.uniroma3.siw.repository.GiocatoreRepository;
+import jakarta.validation.Valid;
+
+@Controller
+public class GiocatoreController {
+	@Autowired
+	GiocatoreRepository giocatoreRepository;
+	
+	@GetMapping("/admin/formAggiungiGiocatore")
+	public String formAggiungiGiocatore(Model model) {
+		model.addAttribute("giocatore", new Giocatore());
+		return "admin/formAggiungiGiocatore.html";
+	}
+	
+	@PostMapping("/admin/aggiungiGiocatore")
+	public String aggiungiGiocatore(@Valid @ModelAttribute("giocatore")Giocatore giocatore, BindingResult bindingResult, Model model) {
+		if(!bindingResult.hasErrors()) {
+			this.giocatoreRepository.save(giocatore);
+			model.addAttribute("giocatore", giocatore);
+			return "admin/giocatore.html";
+		}
+		else {
+			model.addAttribute("messaggioErrore", "Questo giocatore esiste già");
+			return "admin/formAggiungiGiocatore.html";
+		}
+	}
+}
---
+++ src/main/java/it/uniroma3/siw/controller/validator/GiocatoreValidator.java
@@ -0,0 +1,26 @@
+package it.uniroma3.siw.controller.validator;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.validation.Errors;
+
+import it.uniroma3.siw.model.Giocatore;
+import it.uniroma3.siw.repository.GiocatoreRepository;
+public class GiocatoreValidator implements org.springframework.validation.Validator {
+
+	@Autowired
+	GiocatoreRepository giocatoreRepository;
+	@Override
+	public void validate(Object o, Errors errors) {
+		Giocatore giocatore = (Giocatore)o;
+		if (giocatore.getNome()!=null && giocatoreRepository.existsByNomeAndCognomeAndDataDiNascita(giocatore.getNome(), giocatore.getCognome(), giocatore.getDataDiNascita())) {
+			errors.reject("giocatore.duplicate");
+		}
+	}
+	@Override
+	public boolean supports(Class<?> aClass) {
+		return Giocatore.class.equals(aClass);
+	}
+
+	
+
+}
